66f649b2250bc5c36669f3dc7da26201dd07bcd7
Arslan Kirmani <arslankirmani9600@gmail.com>
2015-02-24 00:10:16 +0500

    Add protoU ( Desire X )

--- Android.mk
+++ Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-ifneq ($(filter pico golfu,$(TARGET_DEVICE)),)
+ifneq ($(filter protou pico golfu,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
