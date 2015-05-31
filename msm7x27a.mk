4b6b0bbf047e62f4e747700eb84e0a4c50091c99
Michael Bestas <mikeioannina@gmail.com>
2015-05-31 17:46:31 +0300

    msm7x30-common: Set dalvik.vm.dex2oat-filter=balanced
    * ~11x runtime performance improvement
    
    Change-Id: I108f10518637334fea65351c54f3d3ad2a0de5e2

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -107,7 +107,7 @@ PRODUCT_COPY_FILES += \
 
 # Use ART small mode
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.dex2oat-filter=balanced \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed
 
